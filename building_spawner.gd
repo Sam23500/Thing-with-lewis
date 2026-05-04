72e2ce9aedac2349480a099ebf16430235fba6a7
Azure-Javalin <scilw2007@gmail.com>
2026-05-04 13:00:53 +0100

    Added basic building creator
    Also added timer to friend box

---
+++ building_spawner.gd
@@ -0,0 +1,22 @@
+extends StaticBody3D
+
+func _ready():
+	pass
+
+func _process(delta: float) -> void:
+	# Spawn an object after 2 seconds
+	if Input.is_action_pressed("E"):
+		spawn_object()
+		print("E pressed")
+
+func spawn_object():
+	# 1. Create a new Sprite2D node
+	var sprite = MeshInstance3D.new()
+	# 2. Set its texture
+	var texture = PrismMesh.new()
+	sprite.mesh = texture
+	# 3. Set position
+	sprite.position = Vector3(randi_range(-50,50), -1, randi_range(-50,50))
+	# 4. Add it to the current scene
+	add_child(sprite)
+	print("Spawn attempted")
--- friend_box.gd
+++ friend_box.gd
@@ -1,8 +1,11 @@
 extends StaticBody3D
 
+
+const COOLDOWN: int = 3
 var active: bool = false
 var player: Player
 var follower: PackedScene = load("res://follower.tscn")
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +14,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and timer.is_stopped():
 		var new_follower: Follower = follower.instantiate()
 		new_follower.position.y = 50
 		add_child(new_follower)
+		timer.start(3)
