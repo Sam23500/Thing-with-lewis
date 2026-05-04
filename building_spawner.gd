afb7fa4d16cab899fc3fa1cf7ad1dbb843765dd9
Sam23500 <sam.scott.dot@gmail.com>
2026-05-04 17:13:30 +0100

    Building class and collision

--- building_spawner.gd
+++ building_spawner.gd
@@ -12,19 +12,10 @@ func _ready():
 	pass
 
 func _process(delta: float) -> void:
-	# Spawn an object after 2 seconds
 	if Input.is_action_just_pressed("interact") and active:
-		print("E pressed")
 		spawn_object()
 
 func spawn_object():
-	# 1. Create a new Sprite2D node
-	var sprite = MeshInstance3D.new()
-	# 2. Set its texture
-	var texture = PrismMesh.new()
-	sprite.mesh = texture
-	# 3. Set position
-	sprite.position = Vector3(randi_range(-50,50), -1, randi_range(-50,50))
-	# 4. Add it to the current scene
-	add_child(sprite)
-	print("Spawn attempted")
+	var building = preload("res://building.tscn").instantiate()
+	building.position = Vector3(randi_range(-50,50), -1, randi_range(-50,50))
+	add_child(building)
